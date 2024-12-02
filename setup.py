52f0834f2da92a5253ce114cf8874c31996a144e
Alberto Rota <alberto_rota@outlook.com>
2024-12-02 09:04:33 +0000

    Published Python Package

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ground-control",
+    name="ground-control-tui",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[],  # Add dependencies listed in requirements.txt
     entry_points={
         "console_scripts": [
-            "ground-control = ground_control.monitor:main",  # Change main to your entry function
+            "ground-control-tui = ground_control_tui.monitor:main",  # Change main to your entry function
         ],
     },
     description="A Python Textual app for monitoring VMs in the terminal",
@@ -19,7 +19,7 @@ setup(
     license="GPLv3",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
