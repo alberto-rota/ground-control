6e760fb0ba3b610a18b4d13812cf3f0b0a653910
Zima user <zima@casaos.casaos>
2025-06-25 13:48:17 -0400

    Preparing v1.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ground-control",
+    name="ground-control-tui",
     version="v1.1.0",
     packages=find_packages(),
     install_requires=[
